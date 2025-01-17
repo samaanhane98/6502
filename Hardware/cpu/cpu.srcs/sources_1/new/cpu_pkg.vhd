ceadfeeb7883610254e12f03ad5ba8bacc646ad6
Sam Aanhane <samaanhane@gmail.com>
2025-01-17 21:05:30 +0100

    Added start of project

---
+++ Hardware/cpu/cpu.srcs/sources_1/new/cpu_pkg.vhd
@@ -0,0 +1,36 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 01/17/2025 09:00:25 PM
+-- Design Name: 
+-- Module Name: cpu_pkg - 
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+PACKAGE cpu_pkg IS
+END PACKAGE;
+
+PACKAGE BODY cpu_pkg IS
+END PACKAGE BODY cpu_pkg;
